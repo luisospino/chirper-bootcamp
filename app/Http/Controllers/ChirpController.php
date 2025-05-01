c4f7238e0313ebf26df1e3b46dde43616f0f567c
luis.ospino <luis.ospino@zalvadora.com>
2025-05-01 12:52:55 -0500

    ✨ Agrega el renderizado de la vista con livewire a la ruta /chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -1,14 +1,13 @@
 <?php
 namespace App\Http\Controllers;
-use Illuminate\Http\Response;
-
+use Illuminate\View\View;
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): Response
+    public function index(): View
     {
-        return response('Hello, World!');
+        return view('chirps', []);
     }
 }
