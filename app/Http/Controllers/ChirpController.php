2829e892d5a34ed11838f01121f7df637f01a7c1
luis.ospino <luis.ospino@zalvadora.com>
2025-05-01 12:45:32 -0500

    ✨ Creación del modelo, migración, controlador y enrutamiento básico de los Chirps

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Response;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+        return response('Hello, World!');
+    }
+}
